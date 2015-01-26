c79125e2357c5edc04b5d562fff787a78670bc54
Xi-Plus <a2093064@yahoo.com.tw>
2015-01-26 12:03:49 +0800

    使用CSS樣式表

--- logout/index.php
+++ logout/index.php
@@ -7,12 +7,13 @@ setcookie("ELMScookie", "", time(), "/");
 <head>
 <meta charset="UTF-8">
 <title>登出-TFcisELMS</title>
+<link href="../res/css.css" rel="stylesheet" type="text/css">
 </head>
 <body Marginwidth="-1" Marginheight="-1" Topmargin="0" Leftmargin="0">
 <iframe src="../header.php" width="100%" height="125px" frameborder="0" scrolling="no"></iframe>
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
 	<tr>
-		<td width="50%" height="50" align="center" valign="middle" bgcolor="#0A0" style="color: #FFF; font-size: 24px;">登出中...
+		<td align="center" valign="middle" bgcolor="#0A0" class="message">登出中...
 		</td>
 	</tr>
 </table>
