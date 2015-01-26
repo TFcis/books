2596098976f4d6535efa0bee972bd401bda3d2b3
Xi-Plus <a2093064@yahoo.com.tw>
2015-01-27 07:33:44 +0800

    \func\sql.php 簡化

--- func/sql.php
+++ func/sql.php
@@ -1,13 +1,14 @@
 <?php
+include_once("consolelog.php");
 function sql($query,$select=true){
 	$db=file_get_contents("../config/db.dat");
 	$db=explode("\r\n",$db);
 	$link = mysqli_connect($db[0],$db[1],$db[2],$db[3]);
 	if(mysqli_connect_errno($link)){
-		echo "Failed to connect to MySQL: " . iconv("big5","utf-8",mysqli_connect_error())  ;
+		consolelog("Failed to connect to MySQL: " . iconv("big5","utf-8",mysqli_connect_error()));
 	}
 	$result = mysqli_query($link, $query);
-	echo "<script>console.log(\"".$query."\");</script>";
+	consolelog($query);
 	if($select)return mysqli_fetch_array($result, MYSQLI_NUM);
 }
 ?>
