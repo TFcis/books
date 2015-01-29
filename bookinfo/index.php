7d683487ab7e37c047c15a1cc827ee0ee17b1d62
Xi-Plus <a2093064@yahoo.com.tw>
2015-01-29 17:34:26 +0800

    增加功能 借書 還書 圖書資訊

---
+++ bookinfo/index.php
@@ -0,0 +1,61 @@
+<html>
+<?php
+include_once("../func/sql.php");
+include_once("../func/checklogin.php");
+include_once("../func/consolelog.php");
+include_once("../func/url.php");
+?>
+<head>
+<meta charset="UTF-8">
+<title>圖書資料-TFcisELMS</title>
+</head>
+<body Marginwidth="-1" Marginheight="-1" Topmargin="0" Leftmargin="0">
+<?php
+	include_once("../header.php");
+	$bookinfo=mfa(SELECT("*","booklist",[["id",$_GET["id"]]]));
+?>
+<center>
+<table width="0" border="0" cellspacing="0" cellpadding="0">
+<tr>
+	<td height="50" colspan="2">&nbsp;</td>
+</tr>
+<tr>
+	<td colspan="2" style="text-align: center"><h1>圖書資料</h1></td>
+</tr>
+<tr>
+	<td>
+		<table width="0" border="0" cellspacing="5" cellpadding="0">
+		<tr>
+			<td>ID</td>
+			<td><?php echo $bookinfo["id"]; ?></td>
+		</tr>
+		<tr>
+			<td>書名</td>
+			<td><?php echo $bookinfo["name"]; ?></td>
+		</tr>
+		<tr>
+			<td>分類</td>
+			<td><?php echo mfa(SELECT("*","category",[["id",$bookinfo["cat"]]]))["name"]; ?></td>
+		</tr>
+		<tr>
+			<td>來源</td>
+			<td><?php echo $bookinfo["source"]; ?></td>
+		</tr>
+		</table>
+	</td>
+</tr>
+<tr>
+	<td height="20"></td>
+</tr>
+<tr>
+	<td align="center"><a href="<?php echo $url;?>borrow/?id=<?php echo $bookinfo["id"]; ?>">借閱此書</a>
+	</td>
+</tr>
+<tr>
+	<td align="center"><a href="<?php echo $url;?>return/?id=<?php echo $bookinfo["id"]; ?>">歸還此書</a>
+	</td>
+</tr>
+</table>
+</center>
+</body>
+</html>
---
+++ borrow/index.php
@@ -0,0 +1,99 @@
+<html>
+<?php
+include_once("../func/sql.php");
+include_once("../func/checklogin.php");
+include_once("../func/consolelog.php");
+$error="";
+$message="";
+$data=checklogin();
+if($data==false)header("Location: ../login");
+else if($data[5]<=1){
+	$error="你沒有權限";
+	?><script>setTimeout(function(){history.back();},1000)</script><?php
+}
+if(isset($_POST["bookid"])){
+	if($_POST["bookid"]==""){
+		$error="圖書ID為空";
+	}
+	else if($_POST["borrowuser"]==""){
+		$error="借閱使用者為空";
+	}
+	else{
+		$book=mfa(SELECT("*","booklist",[ ["id",$_POST["bookid"] ] ] ));
+		$acct=mfa(SELECT("*","account",[ ["user",$_POST["borrowuser"] ] ] ));
+		if($book==""){
+			$error="無此圖書ID";
+		}
+		else if($acct==""){
+			$error="無此使用者";
+		}
+		else {
+			UPDATE( "booklist",[["lend",$acct["id"]] ],[["id",$_POST["bookid"]]]);
+			$message="已將圖書 ".$_POST["bookid"]."(".$book["name"].") 借給 ".$acct["user"]."(".$acct["name"].")";
+		}
+	}
+}
+?>
+<head>
+<meta charset="UTF-8">
+<title>借書-TFcisELMS</title>
+<link href="../res/css.css" rel="stylesheet" type="text/css">
+</head>
+<body Marginwidth="-1" Marginheight="-1" Topmargin="0" Leftmargin="0">
+<?php
+	include_once("../header.php");
+	if($error!=""){
+?>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+	<tr>
+		<td align="center" valign="middle" bgcolor="#F00" class="message"><?php echo $error;?></td>
+	</tr>
+</table>
+<?php
+	}
+	if($message!=""){
+?>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+	<tr>
+		<td align="center" valign="middle" bgcolor="#0A0" class="message"><?php echo $message;?></td>
+	</tr>
+</table>
+<?php
+	}
+	if($data["power"]>=2){
+?>
+<center>
+
+<table width="0" border="0" cellspacing="0" cellpadding="0">
+<tr>
+	<td height="50" colspan="1">&nbsp;</td>
+</tr>
+<tr>
+	<td colspan="1" style="text-align: center"><h1>借書</h1></td>
+</tr>
+<tr>
+	<td>
+		<form method="post">
+		<table border="0" cellspacing="5" cellpadding="0">
+		<tr>
+			<td>書本ID</td>
+			<td><input name="bookid" type="text" id="bookid" value="<?php echo $_GET["id"];?>"></td>
+		</tr>
+		<tr>
+			<td>借閱使用者</td>
+			<td><input name="borrowuser" type="text" id="borrowuser"></td>
+		</tr>
+		<tr>
+			<td colspan="2" align="center"><input type="submit" value="借書"></td>
+		</tr>
+		</table>
+		</form>
+	</td>
+</tr>
+</table>
+</center>
+<?php
+	}
+?>
+</body>
+</html>
