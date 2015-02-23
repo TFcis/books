b3a491a2933bc099806eea27e4992e079119cb07
Xi-Plus <a2093064@yahoo.com.tw>
2015-02-23 23:35:41 +0800

    meta currect url

--- res/meta.php
+++ res/meta.php
@@ -5,7 +5,7 @@ function meta($array=null){
 	$meta["title"]="TFcis Books";
 	$meta["type"]="website";
 	$meta["description"]="目前此系統用來管理南一中資訊社TFcis社部內的圖書，若要借書請和社團幹部詢問，所有圖書資訊和出借狀態都可透過本系統查詢。";
-	$meta["url"]="http://".url();
+	$meta["url"]="http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 	$meta["image"]="http://books.tfcis.org/res/icon.png";
 	if($array!=null){
 		foreach($array as $temp){
