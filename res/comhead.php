485429e30719bc6ee2367ca23d779293090caa2a
Xi-Plus <huangxuanyuxiplus@gmail.com>
2016-03-22 09:36:58 +0800

    async js

--- res/comhead.php
+++ res/comhead.php
@@ -12,6 +12,5 @@ $meta=new meta;
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <script src="../res/jquery.min.js"></script>
-<script src="../res/bootstrap/js/bootstrap.min.js"></script>
+<script async src="../res/bootstrap/js/bootstrap.min.js"></script>
 <link href="../res/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<link href="../res/css.css" rel="stylesheet" type="text/css">
